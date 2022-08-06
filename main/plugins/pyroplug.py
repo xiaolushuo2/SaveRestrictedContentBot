860146b2d08ec068e8a23f323e10d5606ddecdee
xiaolushuo2 <xiaolushuo@gmail.com>
2022-08-06 10:31:06 +0800

    Update pyroplug.py
    添加报错输出

--- main/plugins/pyroplug.py
+++ main/plugins/pyroplug.py
@@ -118,7 +118,7 @@ async def get_msg(userbot, client, sender, edit_id, msg_link, i):
             await client.edit_message_text(sender, edit_id, "Have you joined the channel?")
             return 
         except Exception as e:
-            await client.edit_message_text(sender, edit_id, f'Failed to save: `{msg_link}`')
+            await client.edit_message_text(sender, edit_id, f'Failed to save: `{msg_link}`---`{e}`')
             return 
     else:
         edit = await client.edit_message_text(sender, edit_id, "Cloning.")
